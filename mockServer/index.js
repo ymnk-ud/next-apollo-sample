195cdce149bad4c1288bf62408781bd1befc2642
ymnk-ud <yamanaka-yudai@dmm.com>
2020-06-30 17:01:29 +0900

    add: mock apollo-server

---
+++ mockServer/index.js
@@ -0,0 +1,39 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+// スキーマを定義する
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+// クエリで取得するデータを定数で置いておく
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crishton"
+  }
+];
+
+// booksクエリ発行時の処理を指定する
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+};
+
+// サーバーを起動する
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
